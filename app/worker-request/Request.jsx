31af75d014a715410ee87c3197e4b268705f4ae8
JoaquinVietto8 <80827980+JoaquinVietto8@users.noreply.github.com>
2024-08-29 17:16:40 -0300

    Update Request.jsx

--- app/worker-request/Request.jsx
+++ app/worker-request/Request.jsx
@@ -27,6 +27,101 @@ const formatPrice = (price) => {
 const Request = ({ navigation }) => {
   const sheetRef = useRef(null);
   const snapPoints = [225, 500];
+  const [currentSnapIndex, setCurrentSnapIndex] = useState(0);
+
+  const handleSheetChange = (index) => {
+    setCurrentSnapIndex(index); // Actualiza el estado cuando cambia la posición
+  };
+
+  const renderContent = () => {
+    if (currentSnapIndex === 1) {
+      return (
+        <View>
+          <View style={styles.header_detailsContainer}>
+            <Text style={{ fontSize: 18, fontWeight: "bold" }}>
+              Solicitar trabajador
+            </Text>
+            <View style={styles.detailsContainer}>
+              <View style={styles.serviceContainer}>
+                <RightNow />
+                <Text
+                  style={{
+                    marginLeft: 7,
+                    fontSize: 16,
+                  }}
+                >
+                  Electricidad
+                </Text>
+              </View>
+              <View style={styles.priceContainer}>
+                <Text style={{ fontSize: 16, fontWeight: "bold" }}>
+                  {formatPrice("1000")}
+                </Text>
+                <Text style={{ fontSize: 14, color: "#8A8A8A" }}>
+                  Precio de reserva
+                </Text>
+              </View>
+            </View>
+          </View>
+          <View style={styles.dateContainer}>
+            <Pressable
+              style={[
+                styles.date_selectButton,
+                { backgroundColor: nowButtonColor },
+              ]}
+              onPress={() => setSelectedButton("now")}
+            >
+              <RightNow />
+              <Text>Ahora mismo</Text>
+            </Pressable>
+
+            <Pressable
+              style={[
+                styles.date_selectButton,
+                { backgroundColor: scheduleButtonColor },
+              ]}
+              onPress={() => setSelectedButton("schedule")}
+            >
+              <Schedule />
+              <Text>Programado</Text>
+            </Pressable>
+          </View>
+          <View style={styles.descriptionContainer}>
+            <Text style={{ fontWeight: "bold" }}>Describe tu problema</Text>
+            <TextInput
+              style={styles.descriptionInput}
+              maxLength={200}
+              placeholderTextColor="#8A8A8A"
+              selectionColor={"#228dff"}
+              cursorColor="black"
+              multiline={true}
+              value={description}
+              onChangeText={(text) => setDescription(text)}
+            />
+            <Text style={styles.charCount}>
+              {description.length}/{maxLength}
+            </Text>
+          </View>
+        </View>
+      );
+    } else if (currentSnapIndex === 0) {
+      return (
+        <View style={styles.simplifiedContainer}>
+          <View style={styles.simplified}>
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <RightNow />
+              <View style={styles.simplified_schedule}>
+                <RightNow />
+              </View>
+            </View>
+            <Text style={{ fontSize: 16, fontWeight: "bold" }}>
+              {formatPrice("1000")}
+            </Text>
+          </View>
+        </View>
+      );
+    }
+  };
 
   const [selectedButton, setSelectedButton] = useState("now");
   const [nowButtonColor, setNowButtonColor] = useState("#FFCB13");
@@ -81,75 +176,10 @@ const Request = ({ navigation }) => {
           index={1}
           style={styles.bottomSheet}
           handleIndicatorStyle={{ backgroundColor: "#D8D8D8" }}
+          onChange={handleSheetChange}
         >
           <BottomSheetView style={styles.contentContainer}>
-            <View>
-              <View style={styles.header_detailsContainer}>
-                <Text style={{ fontSize: 18, fontWeight: "bold" }}>
-                  Solicitar trabajador
-                </Text>
-                <View style={styles.detailsContainer}>
-                  <View style={styles.serviceContainer}>
-                    <RightNow />
-                    <Text
-                      style={{
-                        marginLeft: 7,
-                        fontSize: 16,
-                      }}
-                    >
-                      Electricidad
-                    </Text>
-                  </View>
-                  <View style={styles.priceContainer}>
-                    <Text style={{ fontSize: 16, fontWeight: "bold" }}>
-                      {formatPrice("1000")}
-                    </Text>
-                    <Text style={{ fontSize: 14, color: "#8A8A8A" }}>
-                      Precio de reserva
-                    </Text>
-                  </View>
-                </View>
-              </View>
-              <View style={styles.dateContainer}>
-                <Pressable
-                  style={[
-                    styles.date_selectButton,
-                    { backgroundColor: nowButtonColor },
-                  ]}
-                  onPress={() => setSelectedButton("now")}
-                >
-                  <RightNow />
-                  <Text>Ahora mismo</Text>
-                </Pressable>
-
-                <Pressable
-                  style={[
-                    styles.date_selectButton,
-                    { backgroundColor: scheduleButtonColor },
-                  ]}
-                  onPress={() => setSelectedButton("schedule")}
-                >
-                  <Schedule />
-                  <Text>Programado</Text>
-                </Pressable>
-              </View>
-              <View style={styles.descriptionContainer}>
-                <Text style={{ fontWeight: "bold" }}>Describe tu problema</Text>
-                <TextInput
-                  style={styles.descriptionInput}
-                  maxLength={200}
-                  placeholderTextColor="#8A8A8A"
-                  selectionColor={"#228dff"}
-                  cursorColor="black"
-                  multiline={true}
-                  value={description}
-                  onChangeText={(text) => setDescription(text)}
-                />
-                <Text style={styles.charCount}>
-                  {description.length}/{maxLength}
-                </Text>
-              </View>
-            </View>
+            {renderContent()}
           </BottomSheetView>
         </BottomSheet>
         <View style={styles.confirmContainer}>
@@ -288,6 +318,30 @@ const styles = StyleSheet.create({
     marginTop: 5,
     textAlign: "right",
     color: "#8A8A8A",
+  },
+  simplifiedContainer: {
+    width: "100%",
+    paddingHorizontal: 20,
+  },
+  simplified: {
+    width: "100%",
+    height: 45,
+    borderWidth: 1,
+    borderColor: "#000",
+    borderRadius: 5,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 10,
+  },
+  simplified_schedule: {
+    backgroundColor: "#FFCB13",
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 6,
+    height: 31,
+    width: 37,
   },
   confirmContainer: {
     borderTopWidth: 0.6,
